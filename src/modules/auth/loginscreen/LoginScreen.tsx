f3ef34279ebd2da7654ce33f5b3c60b537dcf638
saadkhank1616 <115862839+saadkhank1616@users.noreply.github.com>
2024-12-19 10:37:39 +0500

    checking develop

--- src/modules/auth/loginscreen/LoginScreen.tsx
+++ src/modules/auth/loginscreen/LoginScreen.tsx
@@ -13,7 +13,7 @@ import {useNavigation} from '@react-navigation/native';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
-  // Initial commit
+  // develop branch
   return (
     <View style={styles.container}>
       <Spacer height={hp(20)} />
